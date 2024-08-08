7db993ff646e85f16f5d739e9f1514a2271ee849
MgKyaw <mgkyaw20112@gmail.com>
2024-08-08 23:56:46 +0700

    feat: Display OrderIDs that starts with 'B'

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿string[] OrderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+﻿string[] OrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
 foreach (string orderID in OrderIDs)
 {
-    
+    if (orderID.StartsWith('B'))
+    {
+        Console.WriteLine(orderID);
+    }
 }
