468d14ef557edcef04dc7530931c42b4a60045d3
MgKyaw <mgkyaw20112@gmail.com>
2024-08-08 23:53:33 +0700

    feat: Initialize OrderIDs array

--- Program.cs
+++ Program.cs
@@ -1,13 +1,2 @@
-﻿int[] inventory = { 200, 450, 700, 175, 250 };
+﻿string[] OrderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
 
-int sum = 0;
-int bin = 0;
-
-foreach (int item in inventory)
-{
-    sum += item;
-    bin++;
-    Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
-}
-
-Console.WriteLine($"We have {sum} items in inventory.");
