50f78b77a292c36502bdd4dd99ce46d0d031b480
MgKyaw <mgkyaw20112@gmail.com>
2024-08-08 23:49:01 +0700

    feat: Add bin to save foreach count

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿int[] inventory = { 200, 450, 700, 175, 250 };
 
 int sum = 0;
+int bin = 0;
 
 foreach (int item in inventory)
 {
     sum += item;
+    bin++;
 }
 
 Console.WriteLine($"We have {sum} items in inventory.");
